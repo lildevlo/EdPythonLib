f1b6d858540acfa8e0afda53c7bab30fae303e01
lildev <106166359+lildevlo@users.noreply.github.com>
2022-09-07 07:47:55 +0200

    add f+
    ajout connection basique en cours de dev

--- edpylib.py
+++ edpylib.py
@@ -204,4 +204,54 @@ def classe(username, password, storeityesorno, storeityesemplacement):
             f.write(classe)
 
 
+def connect(username, password):
+    print("e")
+    uName = username
+    uPass = password
+
+    print(uName)
+    print(uPass)
+
+    with open('C:/Users/Public/uName.txt', 'w') as f:
+        f.write(uName)
+    with open('C:/Users/Public/uPass.txt', 'w') as f:
+        f.write(uPass)
+
+    with open('C:/Users/Public/uName.txt', encoding="utf-8") as f:
+        username = f.read()
+    with open('C:/Users/Public/uPass.txt', encoding="utf-8") as f:
+        password = f.read()
+
+    conn = http.client.HTTPSConnection("api.ecoledirecte.com")
+    payload = 'data={"identifiant":' + f'"{username}"' + ',"motdepasse":' + f'"{password}"' + '}'
+
+    headers = {
+        'authority': 'api.ecoledirecte.com',
+        'accept': 'application/json, text/plain, */*',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+        'content-type': 'application/x-www-form-urlencoded',
+        'sec-gpc': '1',
+        'origin': 'https://www.ecoledirecte.com',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://www.ecoledirecte.com/',
+        'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'
+    }
+
+    conn.request("POST", "/v3/login.awp", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+
+    error = json.loads(data.decode("utf-8"))['code']
+    error2 = "erreur : " + error
+
+    with open('C:/Users/Public/connectionError.txt', 'w') as f:
+        f.write(error2)
+
+
+
+
+
 
