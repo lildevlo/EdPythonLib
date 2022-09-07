580cbcee6f85ed3b71a74c0e1418549d8d6fb435
lildev <106166359+lildevlo@users.noreply.github.com>
2022-09-07 15:44:49 +0200

    ça avance
     je teste des truc avec l'edt ça marche pas je fais une pause

--- edpylib.py
+++ edpylib.py
@@ -204,7 +204,7 @@ def classe(username, password, storeityesorno, storeityesemplacement):
             f.write(classe)
 
 
-def connect(username, password):
+def connect(username, password, outcodeemplacement):
     print("e")
     uName = username
     uPass = password
@@ -243,15 +243,115 @@ def connect(username, password):
     res = conn.getresponse()
     data = res.read()
 
-
     error = json.loads(data.decode("utf-8"))['code']
-    error2 = "erreur : " + error
+    conv_err = str(error)
 
-    with open('C:/Users/Public/connectionError.txt', 'w') as f:
-        f.write(error2)
+    with open(outcodeemplacement, 'w') as f:
+        f.write(conv_err)
+
+    if error == 200:
+        print("pass")
+    else:
+        print("fail")
+        print(conv_err)
 
 
+def getEDT(username, password, storeityesorno, storeityesemplacement, studentID):
+    if os.path.exists(storeityesemplacement):
+        os.remove(storeityesemplacement)
+    print("e")
+    uName = username
+    uPass = password
+
+    print(uName)
+    print(uPass)
+
+    with open('C:/Users/Public/uName.txt', 'w') as f:
+        f.write(uName)
+    with open('C:/Users/Public/uPass.txt', 'w') as f:
+        f.write(uPass)
+
+    with open('C:/Users/Public/uName.txt', encoding="utf-8") as f:
+        username = f.read()
+    with open('C:/Users/Public/uPass.txt', encoding="utf-8") as f:
+        password = f.read()
+
+    conn = http.client.HTTPSConnection("api.ecoledirecte.com")
+    payload = 'data={"identifiant":' + f'"{username}"' + ',"motdepasse":' + f'"{password}"' + '}'
+
+    headers = {
+        'authority': 'api.ecoledirecte.com',
+        'accept': 'application/json, text/plain, */*',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+        'content-type': 'application/x-www-form-urlencoded',
+        'sec-gpc': '1',
+        'origin': 'https://www.ecoledirecte.com',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://www.ecoledirecte.com/',
+        'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'
+    }
+
+    conn.request("POST", "/v3/login.awp", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    conn.request("POST", "/v3/E/" + studentID + "/emploidutemps.awp?verbe=get", payload, headers)
+
+    r2 = conn.getresponse()
+    edtData = r2.read()
+    conv_edt = str(edtData)
+
+    if storeityesorno == "yes":
+        with open(storeityesemplacement, 'w') as f:
+            f.write(conv_edt)
 
 
+def getID(username, password, storeityesorno, storeityesemplacement):
+    if os.path.exists(storeityesemplacement):
+        os.remove(storeityesemplacement)
+    print("e")
+    uName = username
+    uPass = password
+
+    print(uName)
+    print(uPass)
+
+    with open('C:/Users/Public/uName.txt', 'w') as f:
+        f.write(uName)
+    with open('C:/Users/Public/uPass.txt', 'w') as f:
+        f.write(uPass)
+
+    with open('C:/Users/Public/uName.txt', encoding="utf-8") as f:
+        username = f.read()
+    with open('C:/Users/Public/uPass.txt', encoding="utf-8") as f:
+        password = f.read()
+
+    conn = http.client.HTTPSConnection("api.ecoledirecte.com")
+    payload = 'data={"identifiant":' + f'"{username}"' + ',"motdepasse":' + f'"{password}"' + '}'
+
+    headers = {
+        'authority': 'api.ecoledirecte.com',
+        'accept': 'application/json, text/plain, */*',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+        'content-type': 'application/x-www-form-urlencoded',
+        'sec-gpc': '1',
+        'origin': 'https://www.ecoledirecte.com',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://www.ecoledirecte.com/',
+        'accept-language': 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'
+    }
+
+    conn.request("POST", "/v3/login.awp", payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    ID = json.loads(data.decode("utf-8"))['data']['accounts'][0]['profile']['id']
+    if storeityesorno == "yes":
+        with open(storeityesemplacement, 'w') as f:
+            f.write(ID)
 
 
